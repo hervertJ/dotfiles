c08e6834e5548a74777ff36badcfdf35fbbb5877
Hervert Jimenez <hervertjimenez1@gmail.com>
2026-03-23 18:11:12 -0500

    ...

--- .config/colorschemes/launcher.sh
+++ .config/colorschemes/launcher.sh
@@ -9,14 +9,13 @@ case "$seleccion" in
         ~/.config/colorschemes/apply-theme.sh
         ;;
     " Wallpapers")
-        ~/scripts/wallpapers.sh
+        ~/.config/colorschemes/wallpapers.sh
         ;;
     " Modes")
-        notify-send "Que vas a estudiar hoy?"
-        zen-browser https://www.youtube.com/playlist?list=PLff0yRu6mgBbwNygsHS72Ar044Bccjscz
+        notify-send "pruebas modes"
         ;;
     " Utilities")
-        notify-send "utilities prueba"
+        ~/.config/colorschemes/utilities.sh
         ;;
     " Fonts")
         notify-send "fonts prueba"
