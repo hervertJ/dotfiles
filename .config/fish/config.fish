8f1bd83e3dee90cc21e5a327bcd0b659fbb5b5ed
thinkpadE14 <thinkpadE14@user.com>
2026-04-13 19:08:26 -0500

    feat: first push ThinkpadL14

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,14 +5,15 @@ if status is-interactive
     atuin init fish | source
 
     # dotfiles
-    alias config="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
+    alias config="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME" funcsave config
 
     # eza
-    # alias ls='eza -1 --icons --group-directories-first'
-    #alias ll='eza -lbF --git --icons'
+    alias ls='eza -1 --icons --group-directories-first'
+    alias ll='eza -lbF --git --icons'
     alias ll='eza -lbF --git'
     alias la='eza -lbhHigUmuSa --time-style=long-iso --git --icons'
     alias lt='eza --tree --icons'
+
 
     # zellij
     abbr -a zellij-uni 'zellij --layout ~/.config/zellij/layouts/uni.kdl'
